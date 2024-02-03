eab7734e63cae6023f59e581c635c02936f9e242
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-02-02 23:00:26 -0500

    Update script.js

--- code/static/js/script.js
+++ code/static/js/script.js
@@ -44,15 +44,15 @@ function fetchAndDisplayRecipeDetails(recipeId) {
             // Create HTML for ingredients
             let ingredientsHtml = '<h3>Ingredients:</h3><ul>';
             data.ingredients.forEach(ingredient => {
-                // Check if Quantity is present and not an empty string
-                if(ingredient.Quantity && ingredient.Quantity.trim() !== "") {
+                if (ingredient.Quantity && ingredient.Quantity.trim() !== "") {
                     ingredientsHtml += `<li>${ingredient.Quantity} ${ingredient.Unit} of ${ingredient.Name}</li>`;
                 } else {
-                    // If Quantity is not present, do not include "of"
-                    ingredientsHtml += `<li>${ingredient.Name}</li>`;
+                    // Capitalize the first letter if Quantity is empty
+                    const nameCapitalized = ingredient.Name.charAt(0).toUpperCase() + ingredient.Name.slice(1);
+                    ingredientsHtml += `<li>${nameCapitalized}</li>`;
                 }
             });
-                        ingredientsHtml += '</ul>';
+            ingredientsHtml += '</ul>';
 
             // Create HTML for instructions
             let instructionsHtml = '<h3>Instructions:</h3><ul>';
