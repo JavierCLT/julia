d040e4ab92ac24e7ec110e68df79d24f67fba0b9
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-02-10 01:19:30 -0500

    Update script.js

--- code/static/js/script.js
+++ code/static/js/script.js
@@ -1,91 +1,37 @@
 document.getElementById('search-box').addEventListener('input', function(event) {
     const searchQuery = this.value.trim();
     const resultsContainer = document.getElementById('results');
-    clearTimeout(this.delay);
-    this.delay = setTimeout(() => {
-        if (searchQuery.length > 2) {
-            fetch(`https://jhrecipesapp-1d5375e2a02f.herokuapp.com/search?query=${encodeURIComponent(searchQuery)}`)
-                .then(response => response.json())
-                .then(data => {
-                    resultsContainer.innerHTML = '';
-                    if (!Array.isArray(data)) {
-                        console.error('Received data is not an array:', data);
-                        return;
-                    }
-                    let grid = document.createElement('div');
-                    grid.className = 'grid';
-                    data.forEach(recipe => {
-                        const recipeElement = document.createElement('div');
-                        recipeElement.className = 'recipe-box';
-                        recipeElement.setAttribute('data-recipe-id', recipe.RecipeID); // Set the recipe ID here
-                        recipeElement.innerHTML = `<div class="recipe-content">
-                                                      <h3 class="recipe-title">${recipe.Title}</h3>
-                                                      <!-- Other recipe info can go here -->
-                                                   </div>`;
-                        grid.appendChild(recipeElement);
-                    });
-                    resultsContainer.appendChild(grid);
-                })
-                .catch(error => console.error('Error:', error));
-        } else {
-            resultsContainer.innerHTML = '';
-        }
-    }, 100);
+    if (searchQuery.length > 2) {
+        fetch(`https://foods-cad3aa2b09ba.herokuapp.com/search?query=${encodeURIComponent(searchQuery)}`)
+            .then(response => response.json())
+            .then(data => {
+                resultsContainer.innerHTML = '';
+                let grid = document.createElement('div');
+                grid.className = 'grid';
+                if (data.length > 0) {
+                    const recipe = data[0]; // Assuming only one result for simplicity
+                    const recipeElement = document.createElement('div');
+                    recipeElement.className = 'recipe-box';
+                    recipeElement.innerHTML = `<h3 class="recipe-title" data-id="${recipe.id}">${recipe.title}</h3>`;
+                    recipeElement.onclick = () => fetchAndDisplayFoodDetails(recipe.id);
+                    grid.appendChild(recipeElement);
+                }
+                resultsContainer.appendChild(grid);
+            })
+            .catch(error => console.error('Error:', error));
+    }
 });
 
 // JavaScript function to fetch and display recipe details
-function fetchAndDisplayRecipeDetails(recipeId) {
-    // Fetch the details from the server
-    fetch(`/recipe_details/${encodeURIComponent(recipeId)}`)
+function fetchAndDisplayFoodDetails(id) {
+    fetch(`/recipe_details/${id}`)
         .then(response => response.json())
         .then(data => {
-            // Reference to the container
             const detailsContainer = document.getElementById('recipe-details-container');
-            // Create HTML for ingredients
-            let ingredientsHtml = '<h3>Ingredients:</h3><ul>';
-            data.ingredients.forEach(ingredient => {
-                let quantity = ingredient.Quantity;
-                let unit = ingredient.Unit;
-                let name = ingredient.Name;
-            
-                // Check if quantity is empty and capitalize the first letter of the ingredient's name
-                if (quantity === "") {
-                    name = name.charAt(0).toUpperCase() + name.slice(1);
-                    ingredientsHtml += `<li>${name}</li>`; // No quantity or unit, and 'of' is omitted
-                } else {
-                    // Check if quantity is text and should be capitalized
-                    if (isNaN(quantity)) {
-                        quantity = quantity.charAt(0).toUpperCase() + quantity.slice(1);
-                    }
-                    ingredientsHtml += `<li>${quantity} ${unit} of ${name}</li>`;
-                }
-            });
-            ingredientsHtml += '</ul>';
-
-            // Create HTML for instructions
-            let instructionsHtml = '<h3>Instructions:</h3><ul>';
-            data.instructions.forEach(instruction => {
-                instructionsHtml += `<li>${instruction.Description}</li>`;
-            });
-            instructionsHtml += '</ul>';
-
-            // Empty the container without removing the title
-            let titleElement = document.getElementById('recipe-title');
-            while (titleElement.nextSibling) {
-                detailsContainer.removeChild(titleElement.nextSibling);
-            }
-
-            // Insert the ingredients and instructions HTML after the title
-            document.getElementById('recipe-title').insertAdjacentHTML('afterend', ingredientsHtml + instructionsHtml);
-
-            // Show the container
+            detailsContainer.innerHTML = `<h2>${data.title}</h2><p>${data.explanation}</p>`;
             detailsContainer.style.display = 'block';
-
-            console.log(data);
         })
-        .catch(error => {
-            console.error('Error fetching recipe details:', error);
-        });
+        .catch(error => console.error('Error fetching food details:', error));
 }
 
 function toggleBlurAndOverlay(show) {
