0ab8ef587274e4f731216f4b674a96e6fabda28f
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-04-15 21:18:20 -0400

    Update script.js

--- code/static/js/script.js
+++ code/static/js/script.js
@@ -1,31 +1,12 @@
 // script.js
+import { firebaseConfig } from './config.js';
 
 // Initialize Firebase
-import { firebaseConfig } from './config.js';
-firebase.initializeApp(firebaseConfig);
-
-// Get a reference to the database
+const app = firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
 // Get the search input element
 const searchInput = document.getElementById('search-box');
-
-// Add an event listener to the search input
-searchInput.addEventListener('input', function() {
-  const searchTerm = this.value.trim().toLowerCase();
-
-  // Query the Firebase database for matching recipes
-  const recipesRef = database.ref('recipes');
-  recipesRef.orderByChild('Title').startAt(searchTerm).endAt(searchTerm + '\uf8ff').on('value', function(snapshot) {
-    const searchResults = [];
-    snapshot.forEach(function(childSnapshot) {
-      searchResults.push(childSnapshot.val());
-    });
-
-    // Display the search results on the page
-    displaySearchResults(searchResults);
-  });
-});
 
 // Function to display the search results
 function displaySearchResults(results) {
@@ -38,45 +19,60 @@ function displaySearchResults(results) {
     recipeElement.innerHTML = `
       <h3 class="recipe-title">${recipe.Title}</h3>
     `;
-    recipeElement.addEventListener('click', function() {
-      const recipeId = this.querySelector('.recipe-title').getAttribute('data-id');
-      fetchAndDisplayRecipeDetails(recipeId);
+    // Add event listener for displaying recipe details
+    recipeElement.addEventListener('click', () => {
+      populateRecipeDetails(recipe);
     });
     resultsContainer.appendChild(recipeElement);
   });
 }
 
-// Function to fetch and display recipe details
-function fetchAndDisplayRecipeDetails(recipeId) {
-  const recipeRef = database.ref(`recipes/${recipeId}`);
-  recipeRef.once('value', function(snapshot) {
-    const recipe = snapshot.val();
-    const detailsContainer = document.getElementById('recipe-details-container');
-    const titleElement = document.getElementById('recipe-title');
+// Function to populate recipe details
+function populateRecipeDetails(recipe) {
+  const detailsContainer = document.getElementById('recipe-details-container');
+  const titleElement = document.getElementById('recipe-title');
 
-    titleElement.textContent = recipe.Title;
-    detailsContainer.querySelector('.ingredients-card ul').innerHTML = recipe.Ingredients.map(ingredient => `<li>${ingredient.Name} - ${ingredient.Quantity} ${ingredient.Unit}</li>`).join('');
-    detailsContainer.querySelector('.instructions-card ul').innerHTML = recipe['Numbered Instructions'].map(instruction => `<li>${instruction}</li>`).join('');
+  titleElement.textContent = recipe.Title;
+  detailsContainer.querySelector('.ingredients-card ul').innerHTML = recipe.Ingredients.map(ingredient => `<li>${ingredient.Name} - ${ingredient.Quantity} ${ingredient.Unit}</li>`).join('');
+  detailsContainer.querySelector('.instructions-card ul').innerHTML = recipe['Numbered Instructions'].map(instruction => `<li>${instruction}</li>`).join('');
 
-    detailsContainer.style.display = 'block';
-    toggleBlurAndOverlay(true);
-  });
+  detailsContainer.style.display = 'block';
+  toggleBlurAndOverlay(true);
 }
 
 // Function to toggle blur and overlay
 function toggleBlurAndOverlay(show) {
   const overlay = document.getElementById('darkOverlay');
   const backgroundContent = document.querySelector('.container');
-  if (show) {
-    overlay.style.display = 'block';
-    backgroundContent.classList.add('blur-background');
-  } else {
-    overlay.style.display = 'none';
-    backgroundContent.classList.remove('blur-background');
-  }
+  overlay.style.display = show ? 'block' : 'none';
+  backgroundContent.classList.toggle('blur-background', show);
 }
 
-// Close recipe details when clicking outside the container
+// Add an event listener to the search input
+searchInput.addEventListener('input', function() {
+  const searchTerm = this.value.trim().toLowerCase();
+  
+  clearTimeout(this.delay);
+  this.delay = setTimeout(() => {
+    if (searchTerm.length > 2) {
+      // Fetch and filter recipes from Firebase
+      database.ref('/recipes').once('value').then(snapshot => {
+        const recipes = snapshot.val() ? Object.values(snapshot.val()) : [];
+        const filteredRecipes = recipes.filter(recipe => {
+          return recipe.Title.toLowerCase().includes(searchTerm) ||
+                 recipe.Ingredients.some(ing => ing.Name.toLowerCase().includes(searchTerm)) ||
+                 recipe.Tags.some(tag => tag.toLowerCase().includes(searchTerm)) ||
+                 recipe.Category.some(cat => cat.toLowerCase().includes(searchTerm));
+        });
+        displaySearchResults(filteredRecipes);
+      });
+    } else {
+      document.getElementById('results').innerHTML = '';
+    }
+  }, 300);
+});
+
+// Event listener for closing the recipe details view
 window.addEventListener('click', function(event) {
   const detailsContainer = document.getElementById('recipe-details-container');
   if (!detailsContainer.contains(event.target) && detailsContainer.style.display === 'block') {
