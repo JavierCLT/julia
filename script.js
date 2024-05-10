ba84933ad9766528cd52247c3ff5fe05bc5af92f
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-05-09 22:13:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase
 import { getFirestore, collection, query, where, getDocs } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  // your config values
+  apiKey: "AIzaSyAf_ctPJrXjlrUsmxIlZB2fYsrX4DAJ3Hs",
+  authDomain: "jhrecipes1.firebaseapp.com",
+  databaseURL: "https://jhrecipes1-default-rtdb.firebaseio.com",
+  projectId: "jhrecipes1",
+  storageBucket: "jhrecipes1.appspot.com",
+  messagingSenderId: "319004682120",
+  appId: "1:319004682120:web:787f15928e249e6ea43c3d",
+  measurementId: "G-W9SJVFNSWD"
 };
 
 const app = initializeApp(firebaseConfig);
