a3774eb9a6442e829b525b7f072c918d67a7f982
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-05-14 00:11:41 -0400

    Update app.py

--- code/app.py
+++ code/app.py
@@ -100,4 +100,4 @@ def recipe_details(recipe_id):
     return jsonify(details)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORTG', 8080)))
