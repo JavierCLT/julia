61923c6a7996cccf3203707a2546988d8904a9d9
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-05-15 23:00:44 -0400

    Update script.js

--- code/static/js/script.js
+++ code/static/js/script.js
@@ -56,7 +56,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const handleSearch = debounce(async (event) => {
         const searchQuery = event.target.value.trim();
-        if (searchQuery.length > 2) {
+        if (searchQuery.length > 3) {
             const recipes = await fetchRecipes(searchQuery);
             renderRecipes(recipes);
         } else {
