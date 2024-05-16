18827eb1a496ddad293de6bf90ca573705b7f7dc
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-05-15 22:03:21 -0400

    Update script.js

--- code/static/js/script.js
+++ code/static/js/script.js
@@ -1,76 +1,73 @@
-
-document.getElementById('search-box').addEventListener('input', function(event) {
-    const searchQuery = this.value.trim();
+document.addEventListener('DOMContentLoaded', () => {
+    const searchBox = document.getElementById('search-box');
     const resultsContainer = document.getElementById('results');
-    clearTimeout(this.delay);
-    this.delay = setTimeout(() => {
+    const addRecipeFormContainer = document.getElementById('add-recipe-form-container');
+    const addRecipeForm = document.getElementById('add-recipe-form');
+    const recipeDetailsContainer = document.getElementById('recipe-details-container');
+    const recipeTitle = document.getElementById('recipe-title');
+    const deleteRecipeBtn = document.getElementById('delete-recipe-btn');
+    const darkOverlay = document.getElementById('darkOverlay');
+    const container = document.querySelector('.container');
+
+    const debounce = (func, delay) => {
+        let timer;
+        return (...args) => {
+            clearTimeout(timer);
+            timer = setTimeout(() => func.apply(this, args), delay);
+        };
+    };
+
+    const toggleBlurAndOverlay = (show) => {
+        if (show) {
+            darkOverlay.style.display = 'block';
+            container.classList.add('blur-background');
+        } else {
+            darkOverlay.style.display = 'none';
+            container.classList.remove('blur-background');
+        }
+    };
+
+    const fetchRecipes = async (query) => {
+        try {
+            const response = await fetch(`/search?query=${encodeURIComponent(query)}`);
+            const data = await response.json();
+            return Array.isArray(data) ? data : [];
+        } catch (error) {
+            console.error('Error fetching recipes:', error);
+            return [];
+        }
+    };
+
+    const renderRecipes = (recipes) => {
+        resultsContainer.innerHTML = '';
+        const grid = document.createElement('div');
+        grid.className = 'grid';
+        recipes.forEach(recipe => {
+            const recipeElement = document.createElement('div');
+            recipeElement.className = 'recipe-box';
+            recipeElement.setAttribute('data-recipe-id', recipe.RecipeID);
+            recipeElement.innerHTML = `<div class="recipe-content">
+                                          <h3 class="recipe-title">${recipe.Title}</h3>
+                                       </div>`;
+            grid.appendChild(recipeElement);
+        });
+        resultsContainer.appendChild(grid);
+    };
+
+    const handleSearch = debounce(async (event) => {
+        const searchQuery = event.target.value.trim();
         if (searchQuery.length > 3) {
-            fetch(`/search?query=${encodeURIComponent(searchQuery)}`)
-                .then(response => response.json())
-                .then(data => {
-                    resultsContainer.innerHTML = '';
-                    if (!Array.isArray(data)) {
-                        console.error('Received data is not an array:', data);
-                        return;
-                    }
-                    let grid = document.createElement('div');
-                    grid.className = 'grid';
-                    data.forEach(recipe => {
-                        const recipeElement = document.createElement('div');
-                        recipeElement.className = 'recipe-box';
-                        recipeElement.setAttribute('data-recipe-id', recipe.RecipeID);
-                        recipeElement.innerHTML = `<div class="recipe-content">
-                                                      <h3 class="recipe-title">${recipe.Title}</h3>
-                                                   </div>`;
-                        grid.appendChild(recipeElement);
-                    });
-                    resultsContainer.appendChild(grid);
-                })
-                .catch(error => console.error('Error:', error));
+            const recipes = await fetchRecipes(searchQuery);
+            renderRecipes(recipes);
         } else {
             resultsContainer.innerHTML = '';
         }
-    }, 100);
-});
+    }, 300);
 
-document.getElementById('add-recipe-btn').addEventListener('click', function() {
-    document.getElementById('add-recipe-form-container').style.display = 'block';
-    toggleBlurAndOverlay(true);
-});
-
-document.getElementById('cancel-btn').addEventListener('click', function() {
-    document.getElementById('add-recipe-form-container').style.display = 'none';
-    toggleBlurAndOverlay(false);
-});
-
-document.getElementById('add-recipe-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const formData = new FormData(this);
-    fetch('/add_recipe', {
-        method: 'POST',
-        body: JSON.stringify(Object.fromEntries(formData)),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        alert(data.message);
-        if (data.success) {
-            document.getElementById('add-recipe-form').reset();
-            document.getElementById('add-recipe-form-container').style.display = 'none';
-            toggleBlurAndOverlay(false);
-            // Optionally, refresh the search results or add the new recipe to the results dynamically
-        }
-    })
-    .catch(error => console.error('Error:', error));
-});
-
-function fetchAndDisplayRecipeDetails(recipeId) {
-    fetch(`/recipe_details/${encodeURIComponent(recipeId)}`)
-        .then(response => response.json())
-        .then(data => {
-            const detailsContainer = document.getElementById('recipe-details-container');
+    const fetchAndDisplayRecipeDetails = async (recipeId) => {
+        try {
+            const response = await fetch(`/recipe_details/${encodeURIComponent(recipeId)}`);
+            const data = await response.json();
             let ingredientsHtml = '<h3>Ingredients:</h3><ul>';
             data.ingredients.forEach(ingredient => {
                 ingredientsHtml += `<li>${ingredient.Quantity} ${ingredient.Unit} of ${ingredient.Name}</li>`;
@@ -83,76 +80,93 @@ function fetchAndDisplayRecipeDetails(recipeId) {
             });
             instructionsHtml += '</ul>';
 
-            let titleElement = document.getElementById('recipe-title');
-            while (titleElement.nextSibling) {
-                detailsContainer.removeChild(titleElement.nextSibling);
+            while (recipeTitle.nextSibling) {
+                recipeDetailsContainer.removeChild(recipeTitle.nextSibling);
             }
 
-            document.getElementById('recipe-title').insertAdjacentHTML('afterend', ingredientsHtml + instructionsHtml);
+            recipeTitle.insertAdjacentHTML('afterend', ingredientsHtml + instructionsHtml);
+            recipeDetailsContainer.style.display = 'block';
 
-            detailsContainer.style.display = 'block';
-
-            document.getElementById('delete-recipe-btn').onclick = function() {
+            deleteRecipeBtn.onclick = async () => {
                 const password = prompt("Enter password to delete this recipe:");
                 if (password) {
-                    fetch(`/delete_recipe/${encodeURIComponent(recipeId)}`, {
-                        method: 'POST',
-                        body: JSON.stringify({ password: password }),
-                        headers: {
-                            'Content-Type': 'application/json'
-                        }
-                    })
-                    .then(response => response.json())
-                    .then(data => {
+                    try {
+                        const response = await fetch(`/delete_recipe/${encodeURIComponent(recipeId)}`, {
+                            method: 'POST',
+                            body: JSON.stringify({ password: password }),
+                            headers: {
+                                'Content-Type': 'application/json'
+                            }
+                        });
+                        const data = await response.json();
                         alert(data.message);
                         if (data.success) {
-                            detailsContainer.style.display = 'none';
+                            recipeDetailsContainer.style.display = 'none';
                             toggleBlurAndOverlay(false);
-                            // Optionally, refresh the search results or remove the deleted recipe from the results dynamically
                         }
-                    })
-                    .catch(error => console.error('Error:', error));
+                    } catch (error) {
+                        console.error('Error deleting recipe:', error);
+                    }
                 }
             };
-
-            console.log(data);
-        })
-        .catch(error => {
+        } catch (error) {
             console.error('Error fetching recipe details:', error);
-        });
-}
-
-function toggleBlurAndOverlay(show) {
-    const overlay = document.getElementById('darkOverlay');
-    const backgroundContent = document.querySelector('.container');
-    if (show) {
-        overlay.style.display = 'block';
-        backgroundContent.classList.add('blur-background');
-    } else {
-        overlay.style.display = 'none';
-        backgroundContent.classList.remove('blur-background');
-    }
-}
-
-document.addEventListener('click', function(event) {
-    let targetElement = event.target.closest('.recipe-box');
-    if (targetElement) {
-        const recipeId = targetElement.getAttribute('data-recipe-id');
-        if (recipeId) {
-            const recipeTitle = targetElement.querySelector('.recipe-title').textContent;
-            document.getElementById('recipe-title').textContent = recipeTitle;
-            fetchAndDisplayRecipeDetails(recipeId);
-            toggleBlurAndOverlay(true);
         }
-    }
-});
+    };
 
-window.addEventListener('click', function(event) {
-    const detailsContainer = document.getElementById('recipe-details-container');
-    const recipeTitle = document.getElementById('recipe-title');
-    if (!detailsContainer.contains(event.target) && detailsContainer.style.display === 'block') {
-        detailsContainer.style.display = 'none';
-        recipeTitle.textContent = '';
+    searchBox.addEventListener('input', handleSearch);
+
+    document.getElementById('add-recipe-btn').addEventListener('click', () => {
+        addRecipeFormContainer.style.display = 'block';
+        toggleBlurAndOverlay(true);
+    });
+
+    document.getElementById('cancel-btn').addEventListener('click', () => {
+        addRecipeFormContainer.style.display = 'none';
         toggleBlurAndOverlay(false);
-    }
+    });
+
+    addRecipeForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        const formData = new FormData(addRecipeForm);
+        try {
+            const response = await fetch('/add_recipe', {
+                method: 'POST',
+                body: JSON.stringify(Object.fromEntries(formData)),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            const data = await response.json();
+            alert(data.message);
+            if (data.success) {
+                addRecipeForm.reset();
+                addRecipeFormContainer.style.display = 'none';
+                toggleBlurAndOverlay(false);
+            }
+        } catch (error) {
+            console.error('Error adding recipe:', error);
+        }
+    });
+
+    document.addEventListener('click', (event) => {
+        const targetElement = event.target.closest('.recipe-box');
+        if (targetElement) {
+            const recipeId = targetElement.getAttribute('data-recipe-id');
+            if (recipeId) {
+                const recipeTitleText = targetElement.querySelector('.recipe-title').textContent;
+                recipeTitle.textContent = recipeTitleText;
+                fetchAndDisplayRecipeDetails(recipeId);
+                toggleBlurAndOverlay(true);
+            }
+        }
+    });
+
+    window.addEventListener('click', (event) => {
+        if (!recipeDetailsContainer.contains(event.target) && recipeDetailsContainer.style.display === 'block') {
+            recipeDetailsContainer.style.display = 'none';
+            recipeTitle.textContent = '';
+            toggleBlurAndOverlay(false);
+        }
+    });
 });
