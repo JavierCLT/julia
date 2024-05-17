89e857903fcb58c9c0211aac5a211d827ce40708
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-05-17 00:53:11 -0400

    Update script.js

--- code/static/js/script.js
+++ code/static/js/script.js
@@ -56,7 +56,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const handleSearch = debounce(async (event) => {
         const searchQuery = event.target.value.trim();
-        if (searchQuery.length > 2) {
+        if (searchQuery.length > 3) {
             const recipes = await fetchRecipes(searchQuery);
             renderRecipes(recipes);
         } else {
@@ -159,6 +159,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 fetchAndDisplayRecipeDetails(recipeId);
                 toggleBlurAndOverlay(true);
             }
+        } else if (!addRecipeFormContainer.contains(event.target) && addRecipeFormContainer.style.display === 'block') {
+            addRecipeFormContainer.style.display = 'none';
+            toggleBlurAndOverlay(false);
         }
     });
 
