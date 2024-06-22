7d3c454e8131d12e0f793a0f3e934ab43db4d873
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-06-22 01:07:46 -0400

    Update app.py

--- code/app.py
+++ code/app.py
@@ -20,6 +20,7 @@ cache.init_app(app)
 
 # Secret Key configuration
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY") or os.urandom(24)
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
 
 # Database configuration using environment variables
 db_config = {
